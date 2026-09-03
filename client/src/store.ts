f239c41060fe7dd5afd3758df5afa61a3d3de45a
andervallejolarre <ander.vallejo.larre@gmail.com>
2026-09-03 10:43:40 +0200

    redux setup

---
+++ client/src/store.ts
@@ -0,0 +1,30 @@
+import { configureStore } from '@reduxjs/toolkit'
+import type { Action } from '@reduxjs/toolkit'
+
+interface CounterState {
+  value: number
+}
+
+function counterReducer(state: CounterState = { value: 0 }, action: Action) {
+  switch (action.type) {
+    // Handle actions here
+    default: {
+      return state
+    }
+  }
+}
+
+export const store = configureStore({
+  // Pass in the root reducer setup as the `reducer` argument
+  reducer: {
+    // Declare that `state.counter` will be updated by the `counterReducer` function
+    counter: counterReducer
+  }
+})
+
+// Infer the type of `store`
+export type AppStore = typeof store
+// Infer the `AppDispatch` type from the store itself
+export type AppDispatch = typeof store.dispatch
+// Same for the `RootState` type
+export type RootState = ReturnType<typeof store.getState>
