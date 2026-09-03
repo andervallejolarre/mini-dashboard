cf122513b88c4bcaf5f5a50d7eb933aed4262630
andervallejolarre <ander.vallejo.larre@gmail.com>
2026-09-03 19:24:19 +0200

    countrySelected reducer

--- client/src/features/country/CountrySelect.tsx
+++ client/src/features/country/CountrySelect.tsx
@@ -1,3 +1,6 @@
+import { useAppDispatch } from '../../hooks/index'
+import { type Country, countrySelected } from './countrySlice.ts'
+
 const CountrySelect = () => {
 
     const allCountries = [
@@ -9,12 +12,19 @@ const CountrySelect = () => {
         'bolivia'
     ];
 
+    const dispatch = useAppDispatch()
+    
+    const handleClick = (countrySelection: string)=>{
+    const newCountry : Country = {country: countrySelection};
+    dispatch(countrySelected(newCountry));
+    }
+
     return (
         <section>
             <ul className="countrySelection">
                 <h3>Choose a country:</h3>
                 {allCountries.map(country =>(
-                    <li key={Math.floor(Math.random() * 100)} /*onClick={()=>}*/>{country}</li>
+                    <li key={Math.floor(Math.random() * 100)} onClick={()=> handleClick(country)}>{country}</li>
                 ))}
             </ul>
         </section>
--- client/src/features/country/countrySlice.ts
+++ client/src/features/country/countrySlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit'
 
 export interface Country {
     country: string
@@ -9,8 +9,16 @@ const initialState: Country = { country: 'ecuador'}
 const countrySlice = createSlice({
   name: 'country',
   initialState,
-  reducers: {}
+  reducers: {
+    countrySelected(state, action: PayloadAction<Country>){
+      state.country = action.payload.country
+    }
+  }
 })
+
+
+// Export the auto-generated action creator with the same name
+export const { countrySelected } = countrySlice.actions
 
 // Export the generated reducer function
 export default countrySlice.reducer
